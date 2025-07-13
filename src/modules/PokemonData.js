b18dfac3ca79bc7383fcc5e38861e49745a02974
ysmk <0620komako@gmail.com>
2025-07-13 12:18:39 +0900

    feat: Implement Pokémon status display (#11)

--- src/modules/PokemonData.js
+++ src/modules/PokemonData.js
@@ -1,20 +1,35 @@
 export const extractData = pokemonData => {
-    const { id, name, sprites, types } = pokemonData;
+    const { id, name, sprites, types, stats } = pokemonData;
+    const baseStats = stats.reduce((acc, s) => {
+        acc[s.stat.name] = s.base_stat;
+        return acc;
+    }, {});
+
     return {
         id,
         name,
         img: sprites.front_default,
         types: types.map(t => t.type.name),
+        stats: baseStats,
     };
 };
 
 export const showData = data => {
+    const statsRows = Object.entries(data.stats)
+        .map(([key, val]) => `<tr><th>${key}</th><td>${val}</td></tr>`)
+        .join("");
+
     const html = `
     <dl>
       <dt>Name: ${data.name}</dt>
       <dd><img src="${data.img}" alt=""></dd>
-      <dd>ID: ${data.id}</dd>
+      <dt>ID: ${data.id}</dt>
       <dt>Types: ${data.types.join(", ")}</dt>
+       <table class="stats">
+        <tbody>
+          ${statsRows}
+        </tbody>
+      </table>
     </dl>`;
     document.querySelector("#js-result").innerHTML = html;
 };
